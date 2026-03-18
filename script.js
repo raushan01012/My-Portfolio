505314c3bae38d21593dd1f8f007f1cd203c7009
Raushan kumar <raushanmac73210@gmail.com>
2026-03-19 03:54:28 +0530

    changes

--- script.js
+++ script.js
@@ -201,3 +201,12 @@ if (typingText) {
 }
 
 handleScrollEffects();
+
+
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
